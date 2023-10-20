a753787b1e6e7678af8a4e1dc4a4f3212a9056fa
AgusDOLARD <agust_22_10@hotmail.com>
2023-10-20 23:46:39 +0000

    bye nixos

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,5 +1,6 @@
 return {
 	"ibhagwan/fzf-lua",
+  cmd = "FzfLua",
 	keys = {
 		{ "<M-P>", "<cmd>FzfLua files<cr>" },
 		{ "<M-p>", "<cmd>FzfLua git_files<cr>" },
--- nvim/.config/nvim/lua/plugins/nvimtree.lua
+++ nvim/.config/nvim/lua/plugins/nvimtree.lua
@@ -1,8 +1,17 @@
+
 return {
   "nvim-tree/nvim-tree.lua",
-      cmd = { "NvimTreeToggle"  },
   keys = {
-    { "<M-o>" , "<cmd> NvimTreeToggle<cr>" }
+    { "<M-o>" , function ()
+	local nvimTree=require("nvim-tree.api")
+	local currentBuf = vim.api.nvim_get_current_buf()
+	local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
+	if currentBufFt == "NvimTree" then
+		nvimTree.tree.toggle()
+	else
+		nvimTree.tree.focus()
+	end
+    end }
   },
   config = function ()
     -- code
