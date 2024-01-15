366fe1b6b4c621d3ce0249b94e74f3c420cfc6e9
AgusDOLARD <agust_22_10@hotmail.com>
2024-01-15 01:43:19 -0300

    base16-default-dark

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-    "luisiacc/gruvbox-baby",
+    "RRethy/nvim-base16",
     lazy = false,
     priority = 1000,
     init = function()
-        vim.cmd.colorscheme("gruvbox-baby")
+        vim.cmd.colorscheme("base16-default-dark")
         vim.cmd.highlight("clear SignColumn")
     end,
 }
