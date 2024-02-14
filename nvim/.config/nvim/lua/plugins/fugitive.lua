cc3ead95ada14cede32e74d25a4c0b6c638da792
AgusDOLARD <agust_22_10@hotmail.com>
2024-02-13 23:24:15 -0300

    remap to match shell alias

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
     "tpope/vim-fugitive",
     cmd = { "Git", "G" },
     keys = {
-        { "<leader>gs", "<cmd>tab Git<cr>" },
+        { "gss", "<cmd>tab Git<cr>" },
         { "<leader>gd", vim.cmd.Gvdiffsplit },
     },
 }
