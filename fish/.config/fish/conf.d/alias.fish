c6e8a03579fdf12556c60750f9b3531136b40930
Agus DOLARD <agust_22_10@hotmail.com>
2024-09-23 19:27:13 -0300

    docker aliases

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -1,18 +1,22 @@
 abbr --add byeOrphans 'sudo pacman -Rns $(pacman -Qtdq)'
 abbr --add p "sudo pacman"
 abbr --add cya "rm -rf"
+abbr --add l "ls -alh"
+abbr --add vvd 'cd {$DOTS}'
+alias e='$EDITOR'
+
+# docker
+abbr --add d "docker"
+abbr --add dc "docker compose"
+
+# git
 abbr --add gco "git checkout"
 abbr --add gss "git status"
 abbr --add gwa "git worktree add .wt/"
 abbr --add gwr "git worktree remove .wt/"
 abbr --add glo "git log --oneline --decorate --graph"
-abbr --add l "ls -alh"
-abbr --add lk "walk --icons"
-abbr --add o 'sesh connect $(sesh list | fzf)'
-abbr --add vvd 'cd {$DOTS}'
 
-alias e='$EDITOR'
-alias dps="docker ps --format='table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Size}}\t{{.Status}}'"
+abbr --add o 'sesh connect $(sesh list | fzf)'
 
 function md;
     mkdir -p $argv
